afa7af09d34a91a61bd70553068b450e2a32fbb7
DeepoweRs <bedirhan_kurt_@outlook.com>
2025-04-02 00:05:38 +0200

    feat: bad word filter

--- chat-app/src/components/MessageBar.tsx
+++ chat-app/src/components/MessageBar.tsx
@@ -1,38 +1,65 @@
 import addNewMessage from "@/api/addNewMesssage.ts";
 import {Textarea} from "@/components/ui/textarea.tsx";
 import React, {useState} from "react";
-import {Send} from "lucide-react";
+import {AlertCircle, Send} from "lucide-react";
 import {Button} from "@/components/ui/button.tsx";
+import {Filter} from 'bad-words'
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert.tsx";
 
-export default function MessageBar({ author, bottomRef }: { author: string, bottomRef: React.RefObject<HTMLDivElement | null> }) {
+export default function MessageBar({author, bottomRef}: {
+    author: string,
+    bottomRef: React.RefObject<HTMLDivElement | null>
+}) {
     const [messageContent, setMessageContent] = useState("");
+    const [isProfane, setIsProfane] = useState(false)
 
     const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setMessageContent(e.target.value)
     };
 
     const handleClick = () => {
-        addNewMessage(messageContent, author)
-            .then(() => setMessageContent(""))
-            .catch((err) => console.error(err));
-        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+        const filter = new Filter()
+
+        if (filter.isProfane(messageContent)) {
+            setIsProfane(true)
+        } else {
+            setIsProfane(false)
+            addNewMessage(messageContent, author)
+                .then(() => setMessageContent(""))
+                .catch((err) => console.error(err));
+            bottomRef.current?.scrollIntoView({behavior: "smooth"});
+        }
     };
 
     return (
-        <div className="w-full h-full flex gap-4">
-            <Textarea
-                placeholder="Type your message..."
-                className="resize-none h-2"
-                value={messageContent}
-                onChange={handleMessageChange}
-                onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                        e.preventDefault();
-                        handleClick();
-                    }
-                }}
-            />
-            <Button variant="ghost" className="w-16 h-full" onClick={handleClick}><Send className="size-5" /></Button>
+        <div className='w-full h-full flex flex-col gap-2'>
+            {isProfane ?
+                <Alert variant='destructive' className='border-red-500'>
+                    <AlertCircle className="h-4 w-4" />
+                    <AlertTitle>Be polite!</AlertTitle>
+                    <AlertDescription>
+                        Here is a public chat room. Use a polite language and avoid bad words.
+                    </AlertDescription>
+                </Alert>
+                : null
+            }
+            <div className="flex gap-4">
+                <Textarea
+                    placeholder="Type your message..."
+                    className="resize-none h-2"
+                    value={messageContent}
+                    onChange={handleMessageChange}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            e.preventDefault();
+                            handleClick();
+                        }
+                    }}
+                />
+                <Button variant="ghost" className="w-16 h-full" onClick={handleClick}><Send
+                    className="size-5"/></Button>
+            </div>
         </div>
+
     );
 }
