cf0468972f951c564dfd28fb3a5683ea13227530
DeepoweRs <bedirhan_kurt_@outlook.com>
2025-04-16 16:18:36 +0200

    fix: sign out func

--- chat-app/src/components/AuthRequired.tsx
+++ chat-app/src/components/AuthRequired.tsx
@@ -2,6 +2,7 @@ import {Outlet, useNavigate} from "react-router";
 import useEndSession from "@/hooks/useEndSession.ts";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth} from "@/api/firebaseConfig.ts";
+import {useEffect} from "react";
 
 // Responsible for navigating and session management
 
@@ -10,8 +11,13 @@ export default function AuthRequired() {
   const navigate = useNavigate();
   useEndSession();
 
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+
   if (!user) {
-    navigate("/");
     return null;
   }
 
